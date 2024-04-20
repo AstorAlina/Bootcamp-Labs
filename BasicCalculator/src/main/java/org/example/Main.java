48df2c445deda1121dc17cca555090753059d15f
AstorAlina <astoralina@gmail.com>
2024-04-19 22:35:38 -0500

    Done

--- BasicCalculator/src/main/java/org/example/Main.java
+++ BasicCalculator/src/main/java/org/example/Main.java
@@ -2,14 +2,13 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
 
 
         System.out.println("Enter the first numbers:");
         Scanner scanner = new Scanner(System.in);
         float firstNumber = scanner.nextFloat();
-        scanner.nextLine();
 
         System.out.println("Enter the second number:");
         float secondNumber = scanner.nextFloat();
@@ -19,31 +18,42 @@ public class Main {
         System.out.println("Possible Calculations: (A)dd, (M)ultiply, (S)ubtract or (D)ivide:");
         String mathOption = scanner.nextLine();
 
+
         // block of code to be executed if the condition is true
-        if (mathOption.equalsIgnoreCase ("A")){
-            System.out.println(firstNumber + " + " + secondNumber + " = " +(firstNumber+secondNumber));
+        if (mathOption.equalsIgnoreCase("A")) {
+            System.out.println(firstNumber + " + " + secondNumber + " = " + addTwoNumbers(firstNumber, secondNumber));
 
+        } else if (mathOption.equalsIgnoreCase("S")) {
+            System.out.println(firstNumber + " - " + secondNumber + " = " + subTwoNumbers(firstNumber, secondNumber));
+
+        } else if (mathOption.equalsIgnoreCase("M")) {
+            System.out.println(firstNumber + " * " + secondNumber + " = " + multiTwoNumbers(firstNumber, secondNumber));
+
+        } else if (mathOption.equalsIgnoreCase("D")) {
+            System.out.println(firstNumber + " / " + secondNumber + " = " + divTwoNumbers(firstNumber, secondNumber));
         }
-
-        else if (mathOption.equalsIgnoreCase("M")){
-            System.out.println(firstNumber + " + " + secondNumber + " = " +(firstNumber*secondNumber));
-        }
-
-        else if (mathOption.equalsIgnoreCase("D")){
-            System.out.println(firstNumber + " + " + secondNumber + " = " +(firstNumber/secondNumber));
-        }
-
-        else if (mathOption.equalsIgnoreCase("S")){
-            System.out.println(firstNumber + " + " + secondNumber + " = " +(firstNumber-secondNumber));
-        }
-
-
-
-
-
-
 
 
     }
 
+    // outside of main but inside your class
+    public static float addTwoNumbers(float firstNumber, float secondNumber) {
+        return firstNumber + secondNumber;
+
+    }
+
+    public static float subTwoNumbers(float firstNumber, float secondNumber) {
+        return firstNumber - secondNumber;
+
+    }
+
+    public static float multiTwoNumbers(float firstNumber, float secondNumber) {
+        return firstNumber * secondNumber;
+
+    }
+
+    public static float divTwoNumbers(float firstNumber, float secondNumber) {
+        return firstNumber / secondNumber;
+    }
 }
+//yay
